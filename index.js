c2bf3790d15e9fce09c7912a7f194fd4a2d01231
Edson <edsoncondorimm@gmail.com>
2021-02-28 16:00:52 -0300

    se agrego nodemon

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Prueba");
+
+const base = 2;
+
+
+console.log(`tabla del ${base}`)
+
+for(let i=1; i<10; i++)
+{
+    resultado=`${base}*${i}=${base*i}`
+    console.log(resultado);
+}
